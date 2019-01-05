264a40d2576bef73038d0c09e0d17c7008c26ed0
Jeff Lyon <jeff@albatrossdigital.com>
2019-01-05 15:32:46 -0800

    Update sysvars

--- class/systemconsts.class.php
+++ class/systemconsts.class.php
@@ -1,9 +1,9 @@
 <?php
 class SystemConsts {
-	const DB=getenv('MYSQL_DB'),//'test',
-	HOST=getenv('MYSQL_HOST'),//'localhost',
-    DSN=getenv('CLEARDB_DATABASE_URL'),//'mysql:dbname=test;host=localhost',
-    USERNAME=getenv('MYSQL_USERNAME'),//'root',
-	PASSWORD=getenv('MYSQL_PASSWORD'),//'root';
+	const DB='',//getenv('MYSQL_DB'),//'test',
+	HOST='',//getenv('MYSQL_HOST'),//'localhost',
+    DSN='mysql://b007d5c0106f26:13661656@us-cdbr-iron-east-01.cleardb.net/heroku_d44e1e3216988c9?reconnect=true',//getenv('CLEARDB_DATABASE_URL'),//'mysql:dbname=test;host=localhost',
+    USERNAME='',//getenv('MYSQL_USERNAME'),//'root',
+	PASSWORD='';//getenv('MYSQL_PASSWORD');//'root';
 }
 ?>
